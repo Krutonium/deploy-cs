03a22e8d390ca75b7da80036652b0afe0cf88978
Krutonium <PFCKrutonium@gmail.com>
2022-05-13 22:13:08 -0400

    Don't crash if host is offline

--- deploy-cs/targetCheck.cs
+++ deploy-cs/targetCheck.cs
@@ -7,19 +7,26 @@ public class targetCheck
 {
     internal bool checkIfHostOnline(string host)
     {
-        // Check if host is online by connecting to port 22 and looking for "SSH"
-        TcpClient client = new TcpClient();
-        client.Connect(host, 22);
-        NetworkStream stream = client.GetStream();
-        StreamReader reader = new StreamReader(stream);
-        string ssh = reader.ReadLine();
-        client.Close();
-        if (ssh.Contains("SSH"))
+        try
         {
-            return true;
-        }
-        else
+            // Check if host is online by connecting to port 22 and looking for "SSH"
+            TcpClient client = new TcpClient();
+            client.Connect(host, 22);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            string ssh = reader.ReadLine();
+            client.Close();
+            if (ssh.Contains("SSH"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } catch (Exception e)
         {
+            // The host is not online
             return false;
         }
     }
