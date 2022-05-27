ffc566997161dd8795f4c8de06c1bb16ff09a18b
Krutonium <PFCKrutonium@gmail.com>
2022-05-27 06:52:07 -0400

    Apollo Astronaut

--- deploy-cs/Program.cs
+++ deploy-cs/Program.cs
@@ -16,7 +16,7 @@ namespace deploy_cs // Note: actual namespace depends on the project name.
         //https://nixos.wiki/wiki/Flakes
         static void Main(string[] args)
         {
-            Console.WriteLine("Running Version 1");
+            Console.WriteLine("Running Version Apollo Astronaut");
             Program p = new Program();
             p.BetterMain(args);
         }
