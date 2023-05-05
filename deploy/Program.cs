a27f090a0701d4056c0cdd6445b41ed8093a4b7a
Krutonium <PFCKrutonium@gmail.com>
2023-05-05 02:49:48 -0400

    Force More Newlines?

--- deploy/Program.cs
+++ deploy/Program.cs
@@ -145,8 +145,10 @@ namespace deploy
             Process process = new Process();
             process.StartInfo = psi;
             process.Start();
-            process.OutputDataReceived += (sender, eventArgs) => Console.WriteLine($"{device.Name}: {eventArgs.Data}");
-            process.ErrorDataReceived += (sender, eventArgs) => Console.WriteLine($"{device.Name}: {eventArgs.Data}");
+            process.OutputDataReceived += (sender, eventArgs) =>
+                Console.WriteLine($"{device.Name}: {eventArgs.Data}{Environment.NewLine}");
+            process.ErrorDataReceived += (sender, eventArgs) => 
+                Console.WriteLine($"{device.Name}: {eventArgs.Data}{Environment.NewLine}");
             process.BeginOutputReadLine();
             process.BeginErrorReadLine();
             process.WaitForExit();
